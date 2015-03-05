043c8d2ff7e07fba3daa715b5554e29d0ff303fc
ArturWaz <arturwaz11@gmail.com>
2015-03-05 14:20:52 +0100

    .

---
+++ ClientTCP.h
@@ -0,0 +1,13 @@
+/*
+ * ClientTCP.h
+ *
+ *  Created on: 3/5/2015
+ *      Author: Artur
+ */
+
+
+#ifndef ClientTCP_H_
+#define ClientTCP_H_
+
+
+#endif //__ClientTCP_H_
---
+++ UM7LT.h
@@ -0,0 +1,26 @@
+/*
+ * UM7LT.h
+ *
+ *  Created on: 3/5/2015
+ *      Author: Artur
+ */
+
+
+#ifndef UM7LT_H_
+#define UM7LT_H_
+
+
+#include "base_UM7LT.h"
+
+class UM7LT : private base_UM7LT {
+
+
+
+public:
+
+
+
+};
+
+
+#endif //__UM7LT_H_
